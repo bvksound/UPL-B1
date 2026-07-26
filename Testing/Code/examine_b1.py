0c9f99545f1d3436c6869d6dd3e437988d604880
Peter Turczak <peter@turczak.de>
2026-07-26 14:57:29 +0200

    Document testing a bit more.

--- Testing/Code/examine_b1.py
+++ Testing/Code/examine_b1.py
@@ -2,19 +2,25 @@ import time
 
 import pyvisa
 
-from instruments.hameg import HamegScope
 from instruments.rigol import RigolScope
 from upl_control import Testjig
+
+# Debugging/Diagnostic tool for UPL-B1-Cards
+#
+# Use with a rigol scope (e.g. DS1054) connected via USB
+#
+# The scopes channes are to be connected to the the B1 as follows:
+B1_MAPPING = {1: "P32", 2: "P40", 3: "P53", 4: "P58"}
 
 rm = pyvisa.ResourceManager()
 
 rigol = RigolScope(rm)
-# hameg = HamegScope(rm, "/dev/ttyUSB0")
 jig = Testjig("/dev/ttyACM0")
 
 freq = 0
 
-for freq_range in [0, 1, 2, 4]:
+
+for freq_range in [2, 4]:
     jig.vars["freq_range"] = freq_range
     for coarse in range(16):
         jig.vars["freq_coarse"] = coarse
@@ -26,13 +32,11 @@ for freq_range in [0, 1, 2, 4]:
                 jig.set_state()
                 time.sleep(0.1)
                 # Measure an average of five samples
-                # freqs = [hameg.measure_freq(2) for _ in range(5)]
-                # freq = sum(freqs) / len(freqs)
 
-                # Let the hardware settle for a bit (50 waves at least)
+                # Let the hardware settle for a bit (100 waves at least)
                 while not (1 < freq < 1e6):
                     freq = rigol.measure_freq(1)
-                time.sleep(50 / freq)
+                time.sleep(100 / freq)
                 freq = rigol.measure_freq(1)
 
                 rms = [rigol.measure_rms(ch) for ch in [3, 4]]
