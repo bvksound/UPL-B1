3161aad3d6db08dca3c879adc0077a339fdd48c8
Peter Turczak <peter@turczak.de>
2026-07-26 12:03:00 +0200

    Measure more internal signals for later comparison

--- Testing/Code/examine_b1.py
+++ Testing/Code/examine_b1.py
@@ -3,25 +3,50 @@ import time
 import pyvisa
 
 from instruments.hameg import HamegScope
+from instruments.rigol import RigolScope
 from upl_control import Testjig
 
 rm = pyvisa.ResourceManager()
 
-scope = HamegScope(rm, "/dev/ttyUSB0")
+rigol = RigolScope(rm)
+# hameg = HamegScope(rm, "/dev/ttyUSB0")
 jig = Testjig("/dev/ttyACM0")
+
+freq = 0
 
 for freq_range in [0, 1, 2, 4]:
     jig.vars["freq_range"] = freq_range
     for coarse in range(16):
         jig.vars["freq_coarse"] = coarse
-        for gain in range(0, 0x1000, 0x1FF):
+        for gain in range(100, 0x1000, 0x1FF):
             jig.vars["gain_adj"] = gain
             jig.vars["imd_gain"] = gain
-            for tune in range(0, 0x1000, 0x1FF):
+            for tune in range(100, 0x1000, 0x1FF):
                 jig.vars["freq_tune"] = tune
                 jig.set_state()
                 time.sleep(0.1)
                 # Measure an average of five samples
-                freqs = [scope.measure_freq(2) for _ in range(5)]
-                freq = sum(freqs) / len(freqs)
-                print(freq_range, coarse, gain, tune, freq, flush=True)
+                # freqs = [hameg.measure_freq(2) for _ in range(5)]
+                # freq = sum(freqs) / len(freqs)
+
+                # Let the hardware settle for a bit (50 waves at least)
+                while not (1 < freq < 1e6):
+                    freq = rigol.measure_freq(1)
+                time.sleep(50 / freq)
+                freq = rigol.measure_freq(1)
+
+                rms = [rigol.measure_rms(ch) for ch in [3, 4]]
+                if max(rms) > 1e36:
+                    rigol.autoset()
+                rms = [rigol.measure_rms(ch) for ch in [1, 2, 3, 4]]
+                avg = [rigol.measure_avg(ch) for ch in [1, 2, 3, 4]]
+                print(
+                    freq_range,
+                    coarse,
+                    gain,
+                    tune,
+                    freq,
+                    *rms,
+                    *avg,
+                    flush=True,
+                )
